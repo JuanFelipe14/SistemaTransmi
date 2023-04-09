55231cc5ba8d109fd408640fa271399d688ee55f
Wilermo <william.dmo.sec@gmail.com>
2023-04-09 17:47:54 -0500

    rutas avance

--- src/main/java/com/example/sistematransmilenio/repository/RutaRepository.java
+++ src/main/java/com/example/sistematransmilenio/repository/RutaRepository.java
@@ -1,11 +1,12 @@
 package com.example.sistematransmilenio.repository;
 
 import com.example.sistematransmilenio.model.Ruta;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RutaRepository extends CrudRepository<Ruta, Long> {
+public interface RutaRepository extends JpaRepository<Ruta, Long> {
 
     Ruta findRutaByNombreRuta(String nombreRuta);
 
